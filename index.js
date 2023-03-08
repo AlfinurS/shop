a38b14ef2ba28b4f9a9ee60c6e5891f9a1f5677b
AlfinurS <aaalfinur87@gmail.com>
2023-03-08 14:53:26 +0300

    промежуточный

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class ProductsList{
     constructor(container = '.products'){
         this.container = container;
         this.goods = [];
-        this._fetchProducts();
+        this.allProducts = [];
+        this.getProducts()
+                .then(data => {
+                    this.goods = [...data];
+                    this.render()
+                });
     }
-  
-    _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
+    getProducts(){
+        return fetch(`${API}/catalogData.json`)
+        .then(result => result.json())
+        .catch(error => {
+            console.log(error);
+        })
     }
-    
     render() {
         const block = document.querySelector(this.container);
         for(let product of this.goods){
             const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            //console.log(productObj);
             block.insertAdjacentHTML('beforeend',productObj.render())
         }
     }
 
-    calculateSum(){
-        let sum = 0;
-        for(let product of this.goods){
-            sum += product.title;
-            console.log(sum);
-        }
-    }
 }
 
 class ProductItem{
 	constructor(product, img = 'https://placehold.it/200x150'){
-		this.title = product.title;
+		this.title = product.product_name;
 		this.price = product.price;
-		this.id = product.id;
+		this.id = product.id_product;
 		this.img = img;
+        this.quantity = product.quantity;
 	}
 	
 	render(){
@@ -44,7 +44,9 @@ class ProductItem{
                 <img src="${this.img}" alt="Some img">
                 <h3 class="title">${this.title}</h3>
                 <p class="content">${this.price}</p>
-                <button class="buy-btn">Купить</button>
+                <button class="buy-btn" data-id="${this.id}" 
+                                        data-name="${this.title}"
+                                        data-price="${this.price}">Купить</button>
             </div>`
 	}
 }
@@ -53,48 +55,57 @@ class CartList {
     constructor(container = '.cart'){
         this.container = container;
         this.goods = [];
-    }
-  
-    addProduct(product){
-        this.goods.push(product);
+        this.getCartProducts()
+            .then(data => {
+                this.goods = [...data.contents];
+                this.render()
+            });
+        }
+
+    getCartProducts(){
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     deleteProduct(id){
         this.goods.filter(item => item.id === id);
     }
 
-   
-    
     render() {
         const block = document.querySelector(this.container);
         for(let product of this.goods){
-            const productObj = new ProductItem(product);
-            block.insertAdjacentHTML('beforeend',productObj.render())
+            const productObjCart = new CartItem(product);
+            block.insertAdjacentHTML('afterbegin',productObjCart.render())
         }
     }
+
 }
 
 
 class CartItem {
     constructor(product, img = 'https://placehold.it/200x150'){
-		this.title = product.title;
+		this.title = product.product_name;
 		this.price = product.price;
-		this.id = product.id;
+		this.id = product.id_product;
 		this.img = img;
+        this.quantity = product.quantity;
 	}
-	
+
 	render(){
-		 return `<div class="product-item" data-id="${this.id}">
-                <img src="${this.img}" alt="Some img">
-                <h3 class="title">${this.title}</h3>
+		 return `
+                <h4 class="title">${this.title}</h4>
                 <p class="content">${this.price}</p>
-                <button class="delete-btn">Удалить</button>
-            </div>`
+                <p class="content">${this.quantity}</p>`
 	}
+
 }
+
+//const cart = [];
 const list = new ProductsList();
 list.render();
-list.calculateSum();
 
-/* const cart = new CartList();
-cart.render(); */
+const cart = new CartList();
+cart.render();
