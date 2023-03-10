f72b96539f4c5f552b27e0013e78a5ea42d77df0
AlfinurS <aaalfinur87@gmail.com>
2023-03-10 07:47:43 +0300

    addProduct

--- index.js
+++ index.js
@@ -28,18 +28,13 @@ class ProductsList{
         }
     }
     addEventListeners(){
-        const buyButtons = document.querySelectorAll("#buy-btn");
-        buyButtons.forEach(btn => {
-            btn.addEventListener("click", this.addBtnHandler.bind(this));
-        })
-
-    }
-    addBtnHandler(event){
-        const idBtnBuy = event.target.dataset.id;
-        //console.log(this.goods, idBtnBuy);
-        let find = this.goods.find(product => product.id_product === +idBtnBuy);
-        //console.log(find);
-        
+        document.querySelector(this.container).addEventListener('click', e => {
+            if (e.target.classList.contains('buy-btn')){
+                const idBtnBuy = e.target.dataset.id;
+                let product = this.goods.find(product => product.id_product === +idBtnBuy);
+                cart.addProduct(product);
+            }
+        });
     }
 }
 
@@ -63,19 +58,20 @@ class ProductItem{
 	}
 }
 
-class CartList extends ProductsList{
+class CartList {
     constructor(container = '.cart'){
-        super(container)
-        //this.cart = cart;
-        this.getCartProducts()
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this.getProducts()
             .then(data => {
                 this.goods = [...data.contents];
-                this.render()
+                this.render();
+                this.addEventListeners()
             });
-        }
+    }
 
-    getCartProducts(){
-
+    getProducts(){
         return fetch(`${API}/getBasket.json`)
             .then(result => result.json())
             .catch(error => {
@@ -90,18 +86,37 @@ class CartList extends ProductsList{
     render() {
         const block = document.querySelector(this.container);
         for(let product of this.goods){
-            const productObjCart = new CartItem(product);
-            block.insertAdjacentHTML('afterbegin',productObjCart.render())
+            const productObj = new CartItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('afterbegin',productObj.render())
         }
     }
 
+    addEventListeners(){
+        document.querySelector(this.container).addEventListener('click', e => {
+            if (e.target.classList.contains('delete-btn')){
+                const idBtnBuy = e.target.dataset.id;
+                let product = this.goods.find(product => product.id_product === +idBtnBuy);
+                //this.deleteProduct(product);
+                console.log(product)
+            }
+        });
+    }
+
+    addProduct(product) {
+        // По id продука найти в this.allProducts данный продукт, проверить если его нету, то пушить в this.allProducts и выставлять количество = 1. 
+        // Если продукт уже имеется в корзине, найти его индекс в массиве и увеличить у него количество на один.
+        console.log(product, this.allProducts);
+    }
 
 }
 
 
-class CartItem extends ProductItem {
+class CartItem {
     constructor(product, img = 'https://placehold.it/200x150'){
-        super(product, img)
+		this.title = product.product_name;
+		this.price = product.price;
+		this.id = product.id_product;
         this.quantity = product.quantity;
 	}
 
@@ -109,7 +124,12 @@ class CartItem extends ProductItem {
 		 return `
                 <h4 class="title">${this.title}</h4>
                 <p class="content">${this.price}</p>
-                <p class="content">${this.quantity}</p>`
+                <p class="content">${this.quantity}</p>
+                <button class="delete-btn" id="delete-btn" data-id="${this.id}"
+                    data-name="${this.title}"
+                    data-price="${this.price}"
+                >Удалить</button>
+                `
 	}
 
 }
